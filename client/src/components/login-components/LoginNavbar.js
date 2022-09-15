8fed95772c9069c1e17dc2a341a2d722859d7588
dejanmccreery <108457094+dejanmccreery@users.noreply.github.com>
2022-09-15 15:40:57 +0100

    basic login and register structure

---
+++ client/src/components/login-components/LoginNavbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import DefaultNavbar from '../../Navbar';
+
+export default function LoginNavbar() {
+    return (
+        <div>
+            <button style="background: url(../../../public/static/qa-cinema-logo.png)" />
+        </div>
+    );
+}
---
+++ client/src/pages/Register.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RegForm from '../components/login-components/LoginForm';
+import RegHeader from '../components/login-components/LoginHeader';
+
+export default function Login() {
+    return (
+        <div>
+            <RegHeader />
+            <RegForm />
+        </div>
+    )
+}
