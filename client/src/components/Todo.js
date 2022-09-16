71a1bed01ba625053f01812c31c612bc7f3c996e
Sean <Sean.palla@gmail.com>
2022-09-16 09:57:17 +0100

    #comment updated some merge errors

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./nearbystyles.css";
 
 export default function Todo(props) {
-  return (
-    <div className="object">
-      <p className="Title">{<h3>{props.name}</h3>}</p>
-      <img className="Image" src={props.image} alt={props.alttext} />
-      <p className="desc">{props.desc}</p>
-      <p className="info">{props.info}</p>
-    </div>
-  );
+    return (
+        <div className="object">
+            <p className="Title">{<h3>{props.name}</h3>}</p>
+            <img className="Image" src={props.image} alt={props.alttext} />
+            <p className="desc">{props.desc}</p>
+            <p className="info">{props.info}</p>
+        </div>
+    );
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,10 @@
 import React from "react";
 import LoginForm from "../components/login-components/LoginForm";
 import LoginHeader from "../components/login-components/LoginHeader";
-import LoginNav from "../components/login-components/LoginNavbar";
 
 export default function Login() {
     return (
         <div>
-            <LoginNav />
             <LoginHeader />
             <LoginForm />
         </div>
