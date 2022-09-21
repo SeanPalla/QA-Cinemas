248094da7733e3fd02470752c875b1dbe20daa58
Nikesh Bhudia <nikeshbhudia1@gmail.com>
2022-09-21 11:58:08 +0100

    movie-pages-completed

--- client/src/pages/movies/nowShowing/Deadpool.js
+++ client/src/pages/movies/nowShowing/Deadpool.js
@@ -2,17 +2,26 @@ export default function Deadpool() {
   return (
     <div>
       <h1 className="movieTitle">Deadpool</h1>
-      <h2 className="dateRatingLength"> </h2>
-      <img className="movieImage" src="" alt="" />
+      <h2 className="dateRatingLength">2016 . 15 . 1h 148m </h2>
+      <h3 className="Genre">Action, Adventure, Comedy</h3>
+      <img className="movieImage" src="static/images/Deadpool.png" alt="" />
       <iframe
         className="movieTrailer"
-        title="Deadpool"
-        width="420"
+        width="560"
         height="315"
-        scr="link"></iframe>
-      <p className="Director">fsddddd</p>
-      <p className="Actors"></p>
-      <p className="Desc"></p>
+        src="https://www.youtube.com/embed/Xithigfg7dA"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+      <p className="Director">Director: Tim Miller</p>
+      <p className="Actors">
+        Actors: Ryan Reynolds, Morena Baccarin, T.J. Miller
+      </p>
+      <p className="Desc">
+        A wisecracking mercenary gets experimented on and becomes immortal but
+        ugly, and sets out to track down the man who ruined his looks.
+      </p>
       <p className="rating"></p>
     </div>
   );
--- client/src/pages/movies/nowShowing/Jaws.js
+++ client/src/pages/movies/nowShowing/Jaws.js
@@ -2,17 +2,27 @@ export default function Jaws() {
   return (
     <div>
       <h1 className="movieTitle">Jaws</h1>
-      <h2 className="dateRatingLength"> </h2>
-      <img className="movieImage" src="" alt="" />
+      <h2 className="dateRatingLength">1975 . A . 2h 4m</h2>
+      <h3 className="Genre"> Adventure, Thriller</h3>
+      <img className="movieImage" src="static/images/Jaws.png" alt="" />
       <iframe
         className="movieTrailer"
-        title="jaws"
-        width="420"
+        width="560"
         height="315"
-        scr="link"></iframe>
-      <p className="Director"></p>
-      <p className="Actors"></p>
-      <p className="Desc"></p>
+        src="https://www.youtube.com/embed/U1fu_sA7XhE"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+      <p className="Director">Director: Steven Spielberg</p>
+      <p className="Actors">
+        Actors: Roy Scheider, Robert Shaw, Richard Dreyfuss
+      </p>
+      <p className="Desc">
+        When a killer shark unleashes chaos on a beach community off Cape Cod,
+        it's up to a local sheriff, a marine biologist, and an old seafarer to
+        hunt the beast down.
+      </p>
       <p className="rating"></p>
     </div>
   );
