8f2dbabd376aa7489feeebfe702981662aa6f076
Sean <Sean.palla@gmail.com>
2022-09-22 14:32:26 +0100

    Updated css for the footers

--- client/src/components/OpeningTimes/OpeningTimes.js
+++ client/src/components/OpeningTimes/OpeningTimes.js
@@ -1,27 +1,37 @@
 import React from "react";
 import "./OpeningTimes.css";
 import Container from "react-bootstrap/Container";
-import { BsFillClockFill } from 'react-icons/bs'
+import { BsFillClockFill } from "react-icons/bs";
 
 const OpeningHours = () => {
-
-  return (
-    <Container>
-
-      <br></br>
-      <h1  className="h1-opening-hours" > < BsFillClockFill /> Opening Times </h1>
-      <br></br>
-
-      <h6 className="h6-opening-hours"> Monday - 9:00am-10:00pm </h6>
-      <h6 className="h6-opening-hours"> Tuesday - 9:00am-10:00pm </h6>
-      <h6 className="h6-opening-hours"> Wednesday - 9:00am-10:00pm </h6>
-      <h6 className="h6-opening-hours"> Thursday - 9:00am-10:00pm </h6>
-      <h6 className="h6-opening-hours"> Friday - 9:00am-10:00pm </h6>
-      <h6 className="h6-opening-hours"> Saturday - 10:00am-10:00pm </h6>
-      <h6 className="h6-opening-hours"> Sunday - 11:00am-10:00pm </h6>
-
-    </Container>
-  );
+    return (
+        <Container>
+            <br></br>
+            <h1 className="h1-opening-hours">
+                {" "}
+                <BsFillClockFill /> Opening Times{" "}
+            </h1>
+            <br></br>
+            <div className="openingDiv">
+                <h6 className="h6-opening-hours"> Monday - 9:00am-10:00pm </h6>
+                <h6 className="h6-opening-hours"> Tuesday - 9:00am-10:00pm </h6>
+                <h6 className="h6-opening-hours">
+                    {" "}
+                    Wednesday - 9:00am-10:00pm{" "}
+                </h6>
+                <h6 className="h6-opening-hours">
+                    {" "}
+                    Thursday - 9:00am-10:00pm{" "}
+                </h6>
+                <h6 className="h6-opening-hours"> Friday - 9:00am-10:00pm </h6>
+                <h6 className="h6-opening-hours">
+                    {" "}
+                    Saturday - 10:00am-10:00pm{" "}
+                </h6>
+                <h6 className="h6-opening-hours"> Sunday - 11:00am-10:00pm </h6>
+            </div>
+        </Container>
+    );
 };
 
 export default OpeningHours;
--- client/src/pages/Pay.js
+++ client/src/pages/Pay.js
@@ -5,92 +5,97 @@ import Select from "react-select";
 import ".//paymentStyle.css";
 
 function Payment() {
-  const [showItem, setShowItem] = useState(false);
-  const [paymentData, setPaymentData] = useState({
-    aticket: 0,
-    cticket: 0,
-    kticket: 0,
-  });
+    const [showItem, setShowItem] = useState(false);
+    const [paymentData, setPaymentData] = useState({
+        aticket: 0,
+        cticket: 0,
+        kticket: 0,
+    });
 
-  const handleTicketData = (e) => {
-    if (e.target.value < 0) {
-      alert("Invalid Input, please enter value greater than 0.");
-    } else setPaymentData({ ...paymentData, [e.target.name]: e.target.value });
-  };
+    const handleTicketData = (e) => {
+        if (e.target.value < 0) {
+            alert("Invalid Input, please enter value greater than 0.");
+        } else
+            setPaymentData({ ...paymentData, [e.target.name]: e.target.value });
+    };
 
-  const options = [
-    { value: "BlackAdam", label: "Black Adam" },
-    { value: "Deadpool", label: "Deadpool" },
-    { value: "HowToTrainYourDragon", label: "How To Train Your Dragon" },
-    { value: "Jaws", label: "Jaws" },
-    { value: "ShawshankRedemption", label: "Shawshank Redemption" },
-  ];
+    const options = [
+        { value: "BlackAdam", label: "Black Adam" },
+        { value: "Deadpool", label: "Deadpool" },
+        { value: "HowToTrainYourDragon", label: "How To Train Your Dragon" },
+        { value: "Jaws", label: "Jaws" },
+        { value: "ShawshankRedemption", label: "Shawshank Redemption" },
+    ];
 
-  return (
-    <div className="Payment">
-      {showItem ? (
-        <StripeContainer paymentData={paymentData} />
-      ) : (
-        <>
-          <div1 className="SelectMovieDiv">
-            <h1 className="SelectMovie">Select Movie</h1>
-            <Select className="SelectMovieOption" options={options} />
-          </div1>
-          <form className="PaymentForm">
-            <h1 className="SelectTickets">Select Tickets</h1>
-            <label className="AdultTicket">
-              Adult Tickets:
-              <br />
-              <input
-                className="AdultInput"
-                value={paymentData.aticket}
-                type="number"
-                name="aticket"
-                onChange={handleTicketData}
-                min="0"
-              />
-              <br />
-            </label>
-            <br />
-            <label className="ChildTicket">
-              Child Tickets:
-              <br />
-              <input
-                className="ChildInput"
-                value={paymentData.cticket}
-                type="number"
-                name="cticket"
-                onChange={handleTicketData}
-                min="0"
-              />
-              <br />
-            </label>
-            <br />
-            <label className="ConTicket">
-              Concessionary Tickets:
-              <br />
-              <input
-                className="ConInput"
-                value={paymentData.kticket}
-                type="number"
-                name="kticket"
-                onChange={handleTicketData}
-                min="0"
-              />
-              <br />
-            </label>
-            <br />
-            <button
-              className="PurchaceButton"
-              type="button"
-              onClick={() => setShowItem(true)}>
-              Purchase Tickets
-            </button>
-          </form>
-        </>
-      )}
-    </div>
-  );
+    return (
+        <div className="Payment">
+            {showItem ? (
+                <StripeContainer paymentData={paymentData} />
+            ) : (
+                <>
+                    <div className="SelectMovieDiv">
+                        <h1 className="SelectMovie">Select Movie</h1>
+                        <Select
+                            className="SelectMovieOption"
+                            options={options}
+                        />
+                    </div>
+                    <form className="PaymentForm">
+                        <h1 className="SelectTickets">Select Tickets</h1>
+                        <label className="AdultTicket">
+                            Adult Tickets:
+                            <br />
+                            <input
+                                className="AdultInput"
+                                value={paymentData.aticket}
+                                type="number"
+                                name="aticket"
+                                onChange={handleTicketData}
+                                min="0"
+                            />
+                            <br />
+                        </label>
+                        <br />
+                        <label className="ChildTicket">
+                            Child Tickets:
+                            <br />
+                            <input
+                                className="ChildInput"
+                                value={paymentData.cticket}
+                                type="number"
+                                name="cticket"
+                                onChange={handleTicketData}
+                                min="0"
+                            />
+                            <br />
+                        </label>
+                        <br />
+                        <label className="ConTicket">
+                            Concessionary Tickets:
+                            <br />
+                            <input
+                                className="ConInput"
+                                value={paymentData.kticket}
+                                type="number"
+                                name="kticket"
+                                onChange={handleTicketData}
+                                min="0"
+                            />
+                            <br />
+                        </label>
+                        <br />
+                        <button
+                            className="PurchaceButton"
+                            type="button"
+                            onClick={() => setShowItem(true)}
+                        >
+                            Purchase Tickets
+                        </button>
+                    </form>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default Payment;
