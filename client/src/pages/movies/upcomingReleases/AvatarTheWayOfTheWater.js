5cbf92beaea3b383d3f1d46383d6db76d7327881
Sean <Sean.palla@gmail.com>
2022-09-22 10:37:41 +0100

    added movie pages

--- client/src/pages/movies/upcomingReleases/AvatarTheWayOfTheWater.js
+++ client/src/pages/movies/upcomingReleases/AvatarTheWayOfTheWater.js
@@ -1,19 +1,37 @@
+import "../movieStyle.css";
 export default function AvatarTheWayOfTheWater() {
-  return (
-    <div>
-      <h1 className="movieTitle">Avatar The Way Of The Water</h1>
-      <h2 className="dateRatingLength"> </h2>
-      <img className="movieImage" src="" alt="" />
-      <iframe
-        className="movieTrailer"
-        title="avatar"
-        width="420"
-        height="315"
-        scr="link"></iframe>
-      <p className="Director">fsddddd</p>
-      <p className="Actors"></p>
-      <p className="Desc"></p>
-      <p className="rating"></p>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="movieTitle">Avatar The Way Of The Water</h1>
+            <h2 className="dateRatingLength">2022</h2>
+            <h3 className="genre"> Action - Adventure - Fantasy</h3>
+            <img
+                className="movieImage"
+                src="static/images/Avatar.png"
+                alt="Avatar Poster"
+            />
+            <iframe
+                className="movieTrailer"
+                width="560"
+                height="500"
+                src="https://www.youtube.com/embed/NZrX_ES93JA"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            ></iframe>
+            <p className="Director"> Director: James Cameron</p>
+            <p className="Actors">
+                Actors: Zoe Saldana, Michelle Yeoh, Kate Winslet, Sam
+                Worthington, Sigourney Weaver, Oona Chaplin
+            </p>
+            <p className="movieDesc">
+                Jake Sully lives with his newfound family formed on the planet
+                of Pandora. Once a familiar threat returns to finish what was
+                previously started, Jake must work with Neytiri and the army of
+                the Na'vi race to protect their planet.
+            </p>
+            <p className="rating"></p>
+        </div>
+    );
 }
